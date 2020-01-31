768ede93f27840bc7c520407b7f36334e5e82785
stevehanstudio@gmail.com <15229911+stevehanphoto@users.noreply.github.com>
2020-01-31 01:42:57 -0800

    Added PropTypes to Book component

--- src/Book.js
+++ src/Book.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
 import BookShelfChanger from "./BookShelfChanger";
 
 class Book extends Component {
-    render() {
-        return (
-          <div className="book">
-            <div className="book-top">
-              <div
-                className="book-cover"
-                style={{
-                  width: 128,
-                  height: 193,
-                  backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`
-                }}
-              ></div>
-              <BookShelfChanger book={this.props.book} />
-            </div>
-            <div className="book-title">{this.props.book.title}</div>
-            <div className="book-authors">{this.props.book.authors[0]}</div>
-          </div>
-        );
-    }
+  static propTypes = {
+    book: PropTypes.array.isRequired
+  };
+  render() {
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`
+            }}
+          ></div>
+          <BookShelfChanger book={this.props.book} />
+        </div>
+        <div className="book-title">{this.props.book.title}</div>
+        <div className="book-authors">{this.props.book.authors[0]}</div>
+      </div>
+    );
+  }
 }
 
 export default Book
