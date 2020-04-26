90589ac2fc24066406101c37bf55ceec4810a271
Steve Han <15229911+stevehanphoto@users.noreply.github.com>
2020-04-25 22:22:02 -0700

    Fix deployment issue

---
+++ build/precache-manifest.44517f76e8f658fe04f743f329cbec90.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ce146398a93ae108cc32b637a02416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4144de36df4e5e593fc",
+    "url": "/static/css/main.1b0e0761.chunk.css"
+  },
+  {
+    "revision": "37d9f611929cfcd1d5f5",
+    "url": "/static/js/2.506e3247.chunk.js"
+  },
+  {
+    "revision": "a680856a4903381d69a83276068aa227",
+    "url": "/static/js/2.506e3247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4144de36df4e5e593fc",
+    "url": "/static/js/main.0262fe9b.chunk.js"
+  },
+  {
+    "revision": "bfcb6e9126c75244b2c9",
+    "url": "/static/js/runtime-main.697a9177.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
